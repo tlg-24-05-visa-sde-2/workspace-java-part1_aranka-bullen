2ae4e2e79e047c2e625fd215fd2d5ff1a6ab70b5
Aranka Bullen <arankab@tlgcohort.com>
2024-06-27 17:11:16 -0400

    primitive use cases

--- Lab02.2/src/Television.java
+++ Lab02.2/src/Television.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Television {
+/*
+*Applicatuib or system to model the workings of a Televisiion.
+* it has properties/attributes, it has business methos,
+* but No main() methods
+*/
+class Television {
+    //properties/attributes
+    String brand;
+    int volume;
+
+
+    //functions/methods
+    void turnOn() {
+        System.out.printf("Turning on %s TV. Volume at %s.\n", brand, volume);
+    }
+    void turnOff() {
+        System.out.printf("%s TV is shutting down\n", brand);
+    }
+
 }
--- Lab02.2/src/TelevisionClient.java
+++ Lab02.2/src/TelevisionClient.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class TelevisionClient {
+public class TelevisionClient {
+    public static void main(String[] args) {
+        //creating new instances
+        Television tv1 = new Television();
+        Television tv2 = new Television();
+        Television tv3 = new Television();
+
+        //calling methods on new instances
+
+        tv1.brand = "Samsung";
+        tv1.volume = 32;
+        tv1.turnOn();
+
+        tv2.brand = "Sony";
+        tv2.volume = 24;
+        tv2.turnOff();
+
+        //when attributes are not set then values returned should
+        //return null or zero values
+        tv3.turnOff();
+        tv3.turnOn();
+
+
+    }
 }

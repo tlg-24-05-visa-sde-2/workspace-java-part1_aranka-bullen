7fa38df696ee3b2a3ca293623447d33067966eae
Aranka Bullen <arankab@tlgcohort.com>
2024-06-27 11:47:49 -0400

    initial commit

---
+++ Lab01.1/src/HelloWorld.java
@@ -0,0 +1,10 @@
+    /*
+     *Classic Hello World application in Java.
+     *This is a standalone application, i.e., it does not run on an app server.
+     */
+    class HelloWorld {
+        public static void main(String[] args) {
+            System.out.println("Hello from IntelliJ");
+            System.out.println("This is nicer than bare metals programming");
+        }
+    }
